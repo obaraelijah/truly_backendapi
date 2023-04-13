371c8730bcf1c9b4f6442bff0af7559b12b872a6
elijah obara <elijahobara357@gmail.com>
2023-04-13 20:18:23 +0300

    Hot fix

--- truly/settings.py
+++ truly/settings.py
@@ -132,8 +132,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'AUTH_HEADER_TYPES': ('Bearer', ),
--- user/views.py
+++ user/views.py
@@ -1,3 +1,2 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.core.mail import send_mail
+from django.conf import settings
